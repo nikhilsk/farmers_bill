8f952c69393a368d18f0491339e78013c6d9703b
sathvik09 <sathvikparthasarathy@gmail.com>
2020-10-17 11:43:38 +0530

    delete change

--- backend/controller/productController.js
+++
@@ -1,6 +0,0 @@
-const Validation = require('../models/validation'); 
-
-exports.getProducts = (req,res,next)=>{
-    console.log(req.body);
-    res.send("dsagfd");
-}
--- backend/server.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const port = 5000;
-
-//app 
-const {app} = require('./App');
-const server = http.createServer(app);
-
-server.listen(port);
